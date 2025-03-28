dd55b0c41da22db5adeb030bcfe21679686ca815
VictorHashi <victordhill@gmail.com>
2025-03-28 14:04:32 -0300

    Adição dos seeders de TipoMaoDeObra

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             TipoUsuarioSeeder::class,
-            EstadoSeeder::class
+            EstadoSeeder::class,
+            TipoMaoDeObraSeeder::class
         ]);
     }
 }
